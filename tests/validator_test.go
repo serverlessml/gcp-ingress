d2061454b5b8727407cbb06566872e87e45821de
Dmitry Kisler <admin@dkisler.com>
2020-12-05 00:20:26 +0100

    adds a test

--- tests/validator_test.go
+++ tests/validator_test.go
@@ -1,35 +1,42 @@
-package validator_test
+package tests
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/serverlessml/gcp-ingress/validator"
 )
 
-func TestIsSHA1(t *testing.T) {
+func TestValidator(t *testing.T) {
+	type testStruct struct {
+		Foo string `validate:"required,sha1"`
+	}
+
 	tests := []struct {
 		name string
-		in   string
-		want bool
+		in   testStruct
+		want string
 	}{
 		{
 			name: "Positive",
-			in:   "8c2f3d3c5dd853231c7429b099347d13c8bb2c37",
-			want: true,
+			in:   testStruct{Foo: "8c2f3d3c5dd853231c7429b099347d13c8bb2c37"},
+			want: "",
 		},
 		{
 			name: "Negative",
-			in:   "8c2f3d3c5dd853231c7429b099347d13c8bb2c371",
-			want: false,
+			in:   testStruct{Foo: "8c2f3d3c5dd853231c7429b099347d13c8bb2c371"},
+			want: `Key: 'testStruct.Foo' Error:Field validation for 'Foo' failed on the 'sha1' tag`,
 		},
 	}
+
+	Validator := validator.New()
 
 	for _, test := range tests {
 		t.Run(
 			test.name, func(t *testing.T) {
-				got := validator.IsSHA1(test.in)
-				if got != test.want {
-					t.Fatalf("[%s]: Results don't match\ngot: %v\nwant: %v",
+				got := Validator.Struct(test.in)
+				if reflect.DeepEqual(validator.GetValidationErrors(got), test.want) {
+					t.Fatalf("[%s]: Results don't match\ngot: %s\nwant: %s",
 						test.name, got, test.want)
 				}
 			})
