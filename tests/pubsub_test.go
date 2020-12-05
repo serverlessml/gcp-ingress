ce08bf39dae87cb3f25e1a68a3c3b2831eb5c688
Dmitry Kisler <admin@dkisler.com>
2020-12-05 03:51:42 +0100

    up

---
+++ tests/pubsub_test.go
@@ -0,0 +1,18 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/serverlessml/gcp-ingress/bus"
+)
+
+var client bus.Client
+
+func TestConnect(t *testing.T) {
+	c := client
+	c.ProjectID = "test"
+	err := c.Connect()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
